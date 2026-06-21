8c61226137b685b9bb5be33bbd2e369e1aede1ae
25594 <25594@students.mrgs.school.nz>
2026-06-22 10:27:24 +1200

    making changes

--- Quiz.py
+++ Quiz.py
@@ -2,171 +2,85 @@ import tkinter as tk
 from tkinter import ttk
 
 
-#MAIN APP CLASS
+# --------------------------
+# MAIN APP
+# --------------------------
+
 class QuizApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Extinct Animal Quiz")
         self.root.geometry("700x450")
-        self.root.configure( bg= "#e6f2e6")
+        self.root.configure(bg="#e6f2e6")
 
-        #Quia data
+        # Quiz Data
         self.questions = [
             {"Question": "Which extinct bird was the largest that lived in New Zealand?",
-                "Options": ["Kiwi","Tui","Moa","Kea" ],
-                "answer": "Moa"},
+             "Options": ["Kiwi", "Tui", "Moa", "Kea"],
+             "Answer": "Moa"},
 
+            {"Question": "Which giant eagle once hunted the Moa?",
+             "Options": ["Golden Eagle", "Bald Eagle", "Haast’s Eagle", "Harpy Eagle"],
+             "Answer": "Haast’s Eagle"},
 
-            { "Question": "Which giant eagle once hunted the Moa?",
-                "Options": ["Golden Eagle","Bald Eagle","Haast’s Eagle","Harpy Eagle"],
-                "answer": "Haast’s Eagle"},
+            {"Question": "Approximately when did the Haast’s Eagle become extinct?",
+             "Options": ["50 years ago", "200 years ago", "500-600 years ago", "2000 years ago"],
+             "Answer": "500-600 years ago"},
 
-
-            {"Question": "Approximately when did the Haast’s eagle become Extinct?",
-                "Options": ["50 years ago","200 years ago","500-600 years ago","2000 years ago"],
-                "answer": "500-600 years ago"},
-
-
-            {"Question": "What was the main resource many large birds in New Zealand went extinct?",
-                "Options": ["Volcanos","Human hunting","floods","earth quakes"],
-                "answer": "Human Hunting" },
-
+            {"Question": "What was the main reason many large birds in New Zealand went extinct?",
+             "Options": ["Volcanoes", "Human hunting", "Floods", "Earthquakes"],
+             "Answer": "Human hunting"},
 
             {"Question": "What type of animal was Moa?",
-                "Options": ["Mammals","reptiles","flightless birds","Amphibians"],
-                "answer": "flightless birds"},
+             "Options": ["Mammals", "Reptiles", "Flightless birds", "Amphibians"],
+             "Answer": "Flightless birds"},
 
-            {"Question": "Which Extinct Owl species lived in New Zealand?",
-                "Options": ["Barn Owl","Snowy Owl","Laughing Owl","Forest Owl"],
-                "answer": "Laughing Owl"},
+            {"Question": "Which extinct owl species lived in New Zealand?",
+             "Options": ["Barn Owl", "Snowy Owl", "Laughing Owl", "Forest Owl"],
+             "Answer": "Laughing Owl"},
 
-            {"Question": "What did Haast’s eagle mainly eat?",
-                "Options": ["Fish","insects","Moa","Plants"],
-                "answer": "Moa"},
+            {"Question": "What did Haast’s Eagle mainly eat?",
+             "Options": ["Fish", "Insects", "Moa", "Plants"],
+             "Answer": "Moa"},
 
-
-            {"Question": "When did Moa become Extinct?",
-                "Options": ["Before humans arrived","After humans arrived","During Ice age","Recently"],
-                "answer": "After humans arrived"},
+            {"Question": "When did Moa become extinct?",
+             "Options": ["Before humans arrived", "After humans arrived", "During Ice Age", "Recently"],
+             "Answer": "After humans arrived"},
 
             {"Question": "Which of these animals is extinct in New Zealand?",
-                "Options": ["Kiwi","Moa","Tui","Kea"],
-                "answer": "Moa"},
+             "Options": ["Kiwi", "Moa", "Tui", "Kea"],
+             "Answer": "Moa"},
 
-            {"Question": "Where did Moa lived",
+            {"Question": "Where did Moa live?",
              "Options": ["Australia", "New Zealand", "Africa", "Asia"],
-             "answer": "New Zealand"},
+             "Answer": "New Zealand"}
         ]
 
         self.score = 0
         self.q_index = 0
 
-        #container for pages
-
-        self.container = tk.Frame(root, bg="#e6f2e6")
-        self.container.pack(fill="both", expand=True)
+        container = tk.Frame(root, bg="#e6f2e6")
+        container.pack(fill="both", expand=True)
 
         self.frames = {}
 
-        #Initialize all pages
-        class StartPage(tk.Frame):
-                def __init__(self, parent, app):
-                    super().__init__(parent, bg="#e6f2e6")
-
-                    tk.Label(
-                        self,
-                        text="EXTINCT ANIMAL QUIZ",
-                        font=("Cormorant Garamond", 20, "bold"),
-                        bg="#e6f2e6"
-                    ).pack(pady=20)
-
-                    tk.Button(
-                        self,
-                        text="Start Quiz",
-                        command=lambda: app.show_frame("InstructionPage")
-                    ).pack()
-        class InstructionPage(tk.Frame):
-            def __init__(self, parent, app):
-                super().__init__(parent, bg="#e6f2e6")
-
-                tk.Label(
-                    self,
-                    text="Instructions",
-                    font=("Arial", 18, "bold"),
-                    bg="#e6f2e6"
-                ).pack(pady=20)
-
-                instructions = [
-                    "Read each question carefully",
-                    "Select the correct answer",
-                    "Each correct answer = 1 point",
-                    "Final score shown at the end"
-                ]
-
-                for ins in instructions:
-                    tk.Label(
-                        self,
-                        text="• " + ins,
-                        bg="#e6f2e6",
-                        font=("Arial", 12)
-                    ).pack(anchor="w", padx=100)
-
-                tk.Button(
-                    self,
-                    text="Back",
-                    command=lambda: app.show_frame("StartPage")
-                ).pack(side="left", padx=50, pady=30)
-
-                tk.Button(
-                    self,
-                    text="Start Quiz",
-                    command=lambda: app.show_frame("QuizPage")
-                ).pack(side="right", padx=50, pady=30)
-
-        class QuizPage(tk.Frame):
-            def __init__(self, parent, app):
-                    super().__init__(parent, bg="#e6f2e6")
-
-                    tk.Label(
-                        self,
-                        text="Quiz Page",
-                        font=("Arial", 18, "bold"),
-                        bg="#e6f2e6"
-                    ).pack(pady=20)
-        class ResultPage(tk.Frame):
-            def __init__(self, parent, app):
-                super().__init__(parent, bg="#e6f2e6")
-
-                tk.Label(
-                    self,
-                    text="Quiz Complete!",
-                    font=("Arial", 18, "bold"),
-                    bg="#e6f2e6"
-                ).pack(pady=20)
-
-                self.score_label = tk.Label(
-                    self,
-                    text="Your Score: 0/10",
-                    font=("Arial", 14),
-                    bg="#e6f2e6"
-                )
-                self.score_label.pack(pady=10)
-
-                tk.Button(
-                    self,
-                    text="Play Again",
-                    command=lambda: app.show_frame("StartPage")
-                ).pack(pady=20)
-
-        for F in (StartPage, InstructionPage, QuizPage, ResultPage):
-            frame = F(self.container, self)
-            self.frames[F.__name__] = frame
+        for Page in (StartPage, InstructionPage, QuizPage, ResultPage):
+            frame = Page(container, self)
+            self.frames[Page.__name__] = frame
             frame.place(relwidth=1, relheight=1)
-            self.show_frame("StartPage")
 
-        def show_frame(self, page):
-            frame = self.frames[page]
-            frame.tkraise()
+        self.show_frame("StartPage")
+
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+
+        if page_name == "QuizPage":
+            frame.load_question()
+
+        if page_name == "ResultPage":
+            frame.update_result()
+
+        frame.tkraise()
 
         #---------------------
         #START PAGE
